8403aa7d2e8b92315c0b0c3c2bbc63cfada9f6f0
John D. Johnson II <johnsonjo4531@gmail.com>
2017-04-02 02:23:50 -0600

    first commit

---
+++ readme.md
@@ -0,0 +1,3 @@
+### Async Iterators example
+Code originated from Dr. Axel Rauschmayer's  repository here: https://github.com/rauschma/async-iter-demo/blob/master/src/lines.js
+
---
+++ script.js
@@ -0,0 +1,110 @@
+import {createReadStream} from 'fs';
+
+/**
+ * Takes an element to listen on and listens for all events
+ * only allowing the first triggered event to resolve the 
+ * promise and then cancels all listeners.
+ * 
+ * @returns a promise
+ */
+function onceForAll(el, ...events) {
+  return new Promise((resolve, reject) => {
+    let handlers = {};
+    const handler = (eventName) => function(e) { 
+      resolve([eventName, e]);
+      for(let event of events) {
+        el.removeListener(event, handlers[event]);
+      }
+    };
+    for(let event of events) {
+      if(handlers[event]) {
+          continue;
+      }
+      handlers[event] = handler(event);
+      el.on(event, handlers[event]);
+    }
+  });
+}
+
+/**
+ * Creates an asynchronous ReadStream for the file whose name
+ * is `fileName` and feeds it into an AsyncQueue that it returns.
+ * 
+ * @returns an async iterable 
+ * @see ReadStream https://nodejs.org/api/fs.html#fs_class_fs_readstream
+ */
+async function* readFile(fileName) {
+    const readStream = createReadStream(fileName,
+        { encoding: 'utf8', bufferSize: 1024 });
+    let done = false;
+    while(!done) {
+        yield await onceForAll(readStream, 'data', 'error', 'end')
+            .then(([event, data]) => {
+                switch(event) {
+                    case 'data':
+                        return data.toString('utf8');
+                        break;
+                    case 'error':
+                        return Promise.reject(data);
+                        break;
+                    case 'end':
+                        done = true
+                        return "";
+                        break;
+                }
+            });
+    }
+}
+
+/**
+ * Turns a sequence of text chunks into a sequence of lines
+ * (where lines are separated by newlines)
+ * 
+ * @returns an async iterable 
+ */
+async function* splitLines(chunksAsync) {
+    let previous = '';
+    for await (const chunk of chunksAsync) {
+        previous += chunk;
+        let eolIndex;
+        while ((eolIndex = previous.indexOf('\n')) >= 0) {
+            const line = previous.slice(0, eolIndex);
+            yield line;
+            previous = previous.slice(eolIndex+1);
+        }
+    }
+    if (previous.length > 0) {
+        yield previous;
+    }
+}
+
+/**
+ * @returns an async iterable 
+ */
+async function* numberLines(lines) {
+    let n = 1;
+    try {
+        for await (const line of lines) {
+            yield `${n} ${line}`;
+            n++;
+        }
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+/**
+ * @returns a Promise 
+ */
+async function logLines(asyncIterable) {
+    for await (const line of asyncIterable) {
+        console.log(line);
+    }
+}
+
+async function main() {
+    const fileName = process.argv[2];
+    const asyncIterable = numberLines(splitLines(readFile(fileName)));
+    await logLines(asyncIterable);
+}
+main();
